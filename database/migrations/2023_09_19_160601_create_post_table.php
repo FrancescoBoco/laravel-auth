2c953482a5647c5356afc345e9ce2f2d66160e2a
FrancescoBoco <francesco.bocoux@gmail.com>
2023-09-19 18:53:38 +0200

    ADD seeder fake (Errore su terminale )

--- database/migrations/2023_09_19_160601_create_post_table.php
+++ database/migrations/2023_09_19_160601_create_post_table.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+
 return new class extends Migration
 {
     /**
@@ -14,6 +15,11 @@ return new class extends Migration
         Schema::create('post', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string
+            ('title');
+            $table->string('description');
+            $table->number('age');
+
         });
     }
 

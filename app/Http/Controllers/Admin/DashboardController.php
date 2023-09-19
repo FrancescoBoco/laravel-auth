f0251909f47236a8a201b8352af551fc7f8d726e
FrancescoBoco <francesco.bocoux@gmail.com>
2023-09-19 18:06:37 +0200

    create Migration

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -5,9 +5,15 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Auth;
+
 class DashboardController extends Controller
 {
    public function dashboard(){
+
+    $user = Auth::user();
+    $userId = Auth::id();
+
     return view('admin.dashboard');
    }
 }
